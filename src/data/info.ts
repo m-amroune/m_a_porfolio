064a8cc27bfc7cd35a0896aad2d03895446115e6
m-amroune <amroune.moustapha@gmail.com>
2024-03-25 14:02:31 +0100

    update projects links

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,10 +2,6 @@ export const info = {
   baseUrl: "",
   name: "Bienvenue sur mon site",
   jobDescription: "développeur front-end",
-  about: `Je suis Moustapha Amroune, un développeur front-end bordelais. Suite à une action de formation, je me suis lancé dans l'aventure du développement web, ce qui m'a notamment amené à suivre un parcours de formation développeur front-end  composé de 11 projets professionnalisants chez OpenClassrooms. J'ai pu acquérir un large panel de compétences dans l'univers de la programmation (JavaScript, React, Redux, etc.) et tout ce qui gravite autour (Intégration, Web design, Méthode agile). Je continue à me former en autodidacte sur d'autres technologies (Tailwind, Node.js, etc.) afin de continuer à élargir mes compétences.
-  Vous trouverez sur ce site toutes les réalisations faites au cours de mon cursus de formation.
-   
-  `,
 
   socialMedia: {
     github: "https://github.com/m-amroune",
@@ -13,16 +9,16 @@ export const info = {
     linkedin: "https://www.linkedin.com/in/moustapha-amroune-839986182/",
   },
 
-  competences: [
-    {
-      title:
-        "HRNet (Wealth Health) : Faites passer une librairie jQuery vers React",
-      isFeatured: true,
-      thumbnail: "/assets/images/hrnet.png",
-      githubUrl: "https://github.com/m-amroune/HRNet/tree/main",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
-    },
-  ],
+  // competences: [
+  //   {
+  //     title:
+  //       "HRNet (Wealth Health) : Faites passer une librairie jQuery vers React",
+  //     isFeatured: true,
+  //     thumbnail: "/assets/images/hrnet.png",
+  //     githubUrl: "https://github.com/m-amroune/HRNet/tree/main",
+  //     liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+  //   },
+  // ],
 
   projects: [
     {
@@ -52,7 +48,7 @@ export const info = {
       title: "Kasa : Développez une application Web avec React et React Router",
       isFeatured: true,
       thumbnail: "/assets/images/kasa.png",
-      githubUrl: "hhttps://github.com/m-amroune/kasa_p11",
+      githubUrl: "https://github.com/m-amroune/kasa_p11",
       liveUrl: "https://kasa-m-amroune.netlify.app/",
     },
     {
@@ -60,8 +56,7 @@ export const info = {
         "Learn@Home : Définissez les besoins pour une app de soutien scolaire",
       isFeatured: true,
       thumbnail: "/assets/images/learnhome.png",
-      liveUrl:
-        "https://www.figma.com/file/q3VMIqlZYBsSpw7QDsSw7w/Learn%40Home?type=design&node-id=5-2&mode=design&t=8yluODXU9GxU0Tyx-0",
+      liveUrl: "/assets/images/Amroune_Moustapha_docs_LearnHome.pdf",
     },
     {
       title: "Billed : Débuggez et testez un SaaS RH",
@@ -91,21 +86,21 @@ export const info = {
       isFeatured: true,
       thumbnail: "/assets/images/gameon.png",
       githubUrl: "https://github.com/m-amroune/GameOn-website-FR",
-      liveUrl: "",
+      liveUrl: "https://m-amroune-gameon.netlify.app/",
     },
     {
-      title: "Ohmyfood : Dynamyser une page web avec des animations CSS",
+      title: "Ohmyfood : Dynamiser une page web avec des animations CSS",
       isFeatured: true,
       thumbnail: "/assets/images/ohmyfood.png",
       githubUrl: "https://github.com/m-amroune/MoustaphaAmroune_3_24032021",
-      liveUrl: "https://m-amroune.github.io/MoustaphaAmroune_3_24032021/",
+      liveUrl: "https://m-amroune.github.io/ohmyfood/",
     },
     {
-      title: "Reservia : Transformez une maquette en site web avec HTML & CSS",
+      title: "Reservia : Transformez une maquette en site web avec HTML et CSS",
       isFeatured: true,
       thumbnail: "/assets/images/reservia.png",
       githubUrl: "https://github.com/m-amroune/MoustaphaAmroune_2_24012021",
-      liveUrl: "https://m-amroune.github.io/MoustaphaAmroune_2_24012021/",
+      liveUrl: "https://reservia-m-a.netlify.app/",
     },
   ],
 };
